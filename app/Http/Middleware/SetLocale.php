78177a6399489b45038e9181213c8b4c7317ed2a
Alisher <alishertaubaldy@MacBook-Air-Alisher.local>
2024-05-10 12:41:44 +0500

    lang

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $language = $request->server('HTTP_ACCEPT_LANGUAGE');
+
+        if (isset($language)) {
+            app()->setLocale($language);
+        }
+
+        return $next($request);
+    }
+}
